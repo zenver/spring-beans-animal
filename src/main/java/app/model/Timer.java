64f59198e8e5b37b84dd23eb13d9094b4d436e55
Aleksandr Sosnovskiy <78897318+zenver@users.noreply.github.com>
2021-08-24 10:11:22 +0500

    123
    123

---
+++ src/main/java/app/model/Timer.java
@@ -0,0 +1,12 @@
+package app.model;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Timer {
+
+    private Long nanoTime = System.nanoTime();
+
+    public Long getTime() {
+        return nanoTime;
+    }
+}
